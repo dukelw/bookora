4c17844e256182bbc96fc9b10956707735a996f1
dukelw <thevi16102004@gmail.com>
2025-10-12 15:50:37 +0700

    tunning: change locale file in footer copyright

--- frontend/src/app/layout/default/Footer.tsx
+++ frontend/src/app/layout/default/Footer.tsx
@@ -166,7 +166,7 @@ const AppFooter = () => {
         {/* Bottom bar */}
         <div className="text-center text-gray-500 text-sm border-t border-gray-800 py-4">
           <FooterCopyright
-            by="Bookora Bookstore. All rights reserved."
+            by={t("copyright")}
             year={2025}
           />
         </div>
