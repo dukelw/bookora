37e7096b4be3c7773a4ab4310911cc41058741a2
zjsalt <thuynguyentgdd11112016@gmail.com>
2025-10-28 16:50:53 +0700

    feat: complete all-rating page and add multi-language support for related pages

--- frontend/src/app/[locale]/(client)/orders/page.tsx
+++ frontend/src/app/[locale]/(client)/orders/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import OrderTabs from "./components/OrderTabs";
+import { useTranslations } from "use-intl";
 
 export default function OrdersPage() {
+  const t = useTranslations("order");
   return (
     <div className="min-h-screen bg-[#0b0b0b] text-gray-100 py-12 px-4">
       <div className="max-w-7xl mx-auto">
         <h1 className="text-3xl font-bold mb-10 text-center text-white">
-          Đơn hàng của bạn
+          {t("title")}
         </h1>
 
         <div className="bg-[#111] rounded-2xl shadow-md shadow-yellow-500/10 p-4 md:p-6 border border-gray-800">
--- frontend/src/interfaces/Rating.ts
+++ frontend/src/interfaces/Rating.ts
@@ -2,9 +2,15 @@ export default interface Rating {
   _id: string;
   book: string;
   user: {
+    name: string;
+    avatar: string;
     _id: string;
     fullname: string;
   };
+  variant: {
+    rarity: string;
+    price: number;
+  },
   stars: number;
   comment?: string;
   createdAt: string;
